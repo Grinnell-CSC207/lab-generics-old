bfbe13cf32311ee0fe4c51ed587d80ef6b3ab24c
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-09-29 21:20:41 -0500

    Add a generic box class.

---
+++ src/taojava/generics/Box.java
@@ -0,0 +1,56 @@
+package taojava.generics;
+
+/**
+ * Generic boxes hold shared values.
+ */
+public class Box<T>
+{
+  // +--------+------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  /**
+   * The contents of the box.
+   */
+  T contents;
+
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  /**
+   * Build a new box with specified initial contents.
+   */
+  public Box(T initialContents)
+  {
+    this.contents = initialContents;
+  } // Box(String)
+
+  // +---------+-----------------------------------------------------
+  // | Methods |
+  // +---------+
+
+  /**
+   * Get the contents of the box.
+   */
+  public T get()
+  {
+    return this.contents;
+  } // get()
+
+  /**
+   * Update the contents of the box.
+   */
+  public void set(T newContents)
+  { 
+    this.contents = newContents;
+  } // set(T)
+
+  /**
+   * Get the contents of the box as a string.
+   */
+  public String toString()
+  {
+    return this.contents.toString();
+  } // toString()
+} // class Box<T>
